8c6d4fa28800aab23c7458f3e9d412845bcf494d
neilchencc <93436900+neilchencc@users.noreply.github.com>
2026-08-09 06:59:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from sklearn.linear_model import LinearRegression
 import joblib
 
-pip install streamlit-aggrid
+
 
 # ---------------------------------------------------
 # Title & Introduction
